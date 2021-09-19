4d5054e994ef86f729236cfbebca24e1a8938c20
bacarium <89456754+bacarium@users.noreply.github.com>
2021-09-19 16:31:42 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+let system_loop_counter = 0
 basic.forever(function () {
-	
+    if (system_loop_counter == 9) {
+        system_loop_counter = 0
+    }
+    if (system_loop_counter == 5) {
+        led.plotBrightness(0, 4, 1)
+    }
+    if (system_loop_counter == 9) {
+        led.unplot(0, 4)
+    }
+    if (input.temperature() > 24) {
+        led.plot(2, 2)
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+    } else {
+        led.unplot(2, 2)
+    }
+    basic.pause(100)
+    system_loop_counter += 1
 })
