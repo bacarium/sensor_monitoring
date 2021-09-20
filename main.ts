e618b511cc71fca309d02bafa189782f21a967e4
bacarium <89456754+bacarium@users.noreply.github.com>
2021-09-19 20:03:28 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,17 +3,18 @@ basic.forever(function () {
     if (system_loop_counter > 9) {
         system_loop_counter = 0
     }
-    if (system_loop_counter == 5) {
-        led.plotBrightness(0, 4, 1)
-    }
     if (system_loop_counter == 9) {
         led.unplot(0, 4)
+        led.unplot(4, 4)
     }
     if (input.temperature() > 24) {
-        led.plot(2, 2)
-        music.playTone(262, music.beat(BeatFraction.Eighth))
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        basic.showIcon(IconNames.Chessboard)
     } else {
-        led.unplot(2, 2)
+        basic.clearScreen()
+    }
+    if (system_loop_counter == 5) {
+        led.plotBrightness(0, 4, 1)
     }
     basic.pause(100)
     system_loop_counter += 1
